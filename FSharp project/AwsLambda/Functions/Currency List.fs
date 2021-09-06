3d606aeb975f258bfb35666428a52bfae5a96633
Alex <alessandro.piccione.75@gmail.com>
2021-09-06 23:46:30 +0100

    correct list function

--- FSharp project/AwsLambda/Functions/Currency List.fs
+++ FSharp project/AwsLambda/Functions/Currency List.fs
@@ -9,16 +9,18 @@ type Currency_List () =
     let createOk (body:string) =
         let response = APIGatewayProxyResponse()
         response.StatusCode <- 204
+        response.Body <- body
+        response.Headers.["Content-Type"] = "application/json"
 
     member this.Handle (request:APIGatewayProxyRequest, context:ILambdaContext) = 
 
         // TODO: validate
         // TODO: store in repository
 
-        let body = $@"""[
+        let body = $@"""{{ ""data"": [
             {{ ""Code"":""EUR"", ""Name"": ""Euro"" }},
             {{ ""Code"":""ETH"", ""Name"": ""Ethereum"" }},
-        ]"""
+        ]}}"""
 
         createOk(body)
 
