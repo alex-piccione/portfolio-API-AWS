0717a353685c7429f226490b50a66b0409d44068
Alex <alessandro.piccione.75@gmail.com>
2021-09-06 23:37:35 +0100

    add LambdaSerializer

--- FSharp project/AwsLambda/Library.fs
+++ FSharp project/AwsLambda/Library.fs
@@ -1,5 +1,10 @@
 ﻿namespace AwsLambda
 
+open Amazon.Lambda.Core
+
+[<assembly: LambdaSerializer(typeof<Amazon.Lambda.Serialization.Json.JsonSerializer>)>]
+do ()
+
 module Say =
     let hello name =
         printfn "Hello %s" name
