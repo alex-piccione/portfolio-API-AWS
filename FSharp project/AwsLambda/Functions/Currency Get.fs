49972d8384a71017d2f2d68431c324ac416afbd0
Alex <alessandro.piccione.75@gmail.com>
2021-09-08 01:13:18 +0100

    querystring parameter

--- FSharp project/AwsLambda/Functions/Currency Get.fs
+++ FSharp project/AwsLambda/Functions/Currency Get.fs
@@ -3,10 +3,15 @@
 open Amazon.Lambda.APIGatewayEvents
 open Amazon.Lambda.Core
 open Portfolio.Api.Core
+open Portfolio.Api.MongoRepository
 
 [<Class>]
 type Get (repository:ICurrencyRepository) =
     inherit FunctionBase()
+
+    new () = 
+        let repolsitory = CurrencyRepository()
+        Get(repolsitory)
 
     member this.Handle (request:APIGatewayProxyRequest, context:ILambdaContext) =
         match request.QueryStringParameters.TryGetValue("code") with
--- FSharp project/MongoRepository/CurrencyRepository.fs
+++ FSharp project/MongoRepository/CurrencyRepository.fs
@@ -3,15 +3,15 @@
 open Portfolio.Api.Core
 open Portfolio.Api.Core.Entities
 
-type MongoRepository () =
+type CurrencyRepository () =
 
     interface ICurrencyRepository with
 
         member this.Create(arg1: Currency): unit = 
             ()
 
-        member this.Get(arg1: string): Currency = 
-            { Code="EUR"; Name="Euro"}
+        member this.Get(code: string): Currency = 
+            { Code=code; Name=code.ToLowerInvariant() }
 
         member this.List(): Currency list = 
             [
