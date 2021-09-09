05d05d81ce6752bdbf8140d48a686a7e1a425976
Alex <alessandro.piccione.75@gmail.com>
2021-09-09 21:07:20 +0100

    Create CRUD interface

---
+++ FSharp project/Core/IUserRepository.fs
@@ -0,0 +1,15 @@
+﻿namespace Portfolio.Api.Core
+
+open Portfolio.Api.Core.Entities
+
+type CRUD<'T> =
+    abstract member Create: 'T -> unit
+    abstract member Delete: string -> unit
+    abstract member Update: 'T * string -> unit
+    abstract member Get: string -> 'T
+    abstract member All: unit -> 'T list
+
+type IUserRepository =
+    inherit CRUD<User>
+
+
--- FSharp project/Core/entities.fs
+++ FSharp project/Core/entities.fs
@@ -1,3 +1,15 @@
 ﻿module Portfolio.Api.Core.Entities
 
+open System
+
 type Currency = { Code:string; Name:string}
+
+type User = {
+    Username:string
+    Email:string
+    Password:string
+    PasswordHint:string
+    CreatedOn:DateTime
+    IsEmailValidated:bool
+    IsBlocked:bool
+}
