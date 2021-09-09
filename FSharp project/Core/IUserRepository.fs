94a94fe6e46b3ff418785f7657d120029dc4fc08
Alex <alessandro.piccione.75@gmail.com>
2021-09-09 21:09:54 +0100

    create All repository interface

--- FSharp project/Core/IUserRepository.fs
+++ FSharp project/Core/IUserRepository.fs
@@ -4,12 +4,14 @@ open Portfolio.Api.Core.Entities
 
 type CRUD<'T> =
     abstract member Create: 'T -> unit
-    abstract member Delete: string -> unit
+    abstract member Read: string -> 'T
     abstract member Update: 'T * string -> unit
-    abstract member Get: string -> 'T
+    abstract member Delete: string -> unit
+
+type All<'T> =
     abstract member All: unit -> 'T list
 
 type IUserRepository =
     inherit CRUD<User>
-
+    inherit All<User>
 
