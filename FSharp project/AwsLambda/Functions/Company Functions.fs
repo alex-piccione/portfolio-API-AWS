7ada37dcb1b64f0040d453af9809a6f0a024c659
Alex <alessandro.piccione.75@gmail.com>
2021-10-03 17:10:52 +0100

    WIP

---
+++ FSharp project/AwsLambda/Functions/Company Functions.fs
@@ -0,0 +1,56 @@
+﻿namespace Portfolio.Api.Functions
+
+open Amazon.Lambda.APIGatewayEvents
+open Amazon.Lambda.Core
+open Portfolio.Api.Functions
+open Portfolio.Api.Core
+open Microsoft.Extensions.Configuration
+open Portfolio.Api.MongoRepository
+
+
+type CompanyFunctions (repository:ICompanyRepository) =
+    inherit FunctionBase()
+
+    new () =
+        let configFile = "configuration.json"
+        let variable = "MongoDB_connection_string"
+
+        let configuration = ConfigurationBuilder()
+                                .AddJsonFile(configFile)
+                                .Build()
+
+        let connectionString = configuration.[variable]
+        if connectionString = null then failwith $@"Cannot find ""{variable}"" in ""{configFile}""."
+
+        CurrencyFunctions(CompanyRepository(connectionString))
+
+
+    member this.Create (request:APIGatewayProxyRequest, context:ILambdaContext) =
+        context.Logger.Log $"Create: {request.Body}"
+        try
+            let currency = base.Deserialize request.Body
+            repository.Create(currency)
+            this.createOkWithStatus 201
+        with exc ->
+            context.Logger.Log $"Failed to create Currency. Data: {request.Body}. Error: {exc}"
+            this.createError $"Failed to create Currency. Error: {exc.Message}"
+
+    
+    member this.All (request:APIGatewayProxyRequest, context:ILambdaContext) =
+        context.Logger.Log("All")
+        try
+            let list = repository.All()
+            base.createOkWithData(list)
+        with exc ->
+            context.Logger.Log $"Failed to retrieve Currencies. {exc}"
+            this.createError $"Failed to retrieve Currencies. Error: {exc.Message}"
+
+    member this.Single (request:APIGatewayProxyRequest, context:ILambdaContext) =
+
+        context.Logger.Log($"request.QueryStringParameters: {request.QueryStringParameters}")
+
+        if request.QueryStringParameters = null then this.createError("Missing querystring")
+        else
+            match request.QueryStringParameters.TryGetValue("code") with
+            | (true, id) -> base.createOkWithData (repository.Single id)
+            | _ -> failwith @"Missing querystring parameter ""code""."
---
+++ FSharp project/MongoRepository/CompanyRepository.fs
@@ -0,0 +1,13 @@
+﻿namespace Portfolio.Api.MongoRepository
+
+open Portfolio.Api.Core
+open Portfolio.Api.Core.Entities
+
+type CompanyRepository (connectionString:string) =
+    inherit CrudRepository<Company>(connectionString, "Company", (fun x -> x.Code))
+
+    interface ICompanyRepository with 
+        member this.Create(item: Currency) = base.Create item
+        member this.Delete(id: string) = base.Delete id
+        member this.Single(id: string) = base.Single id
+        member this.Update(item: Currency) = base.Update(item.Code, item)
