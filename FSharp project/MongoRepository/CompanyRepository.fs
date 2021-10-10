a99b33bb71f0ff442d1945ec379c8d8b1d7f2f3b
Alessandro <876490+alex-piccione@users.noreply.github.com>
2021-10-10 11:13:59 +0100

    Implement Exists method on Company repository (#25)
    * Implement Exists method
    
    * cleanup

--- FSharp project/MongoRepository/CompanyRepository.fs
+++ FSharp project/MongoRepository/CompanyRepository.fs
@@ -1,7 +1,9 @@
 ﻿namespace Portfolio.MongoRepository
 
+open System.Linq
 open Portfolio.Core
 open Portfolio.Core.Entities
+open MongoDB.Driver
 open MongoDB.Bson.Serialization
 open serializers
 
@@ -9,12 +11,16 @@ open serializers
 type CompanyRepository (connectionString:string) =
     inherit CrudRepository<Company>(connectionString, "Company", (fun x -> x.Id), CompanyRepository.overloadMap)
 
-    interface ICompanyRepository with 
+    interface ICompanyRepository with
         member this.Create(item: Company) = base.Create item
         member this.Delete(id: string) = base.Delete id
         member this.Single(id: string) = base.Single id
         member this.Update(item: Company) = base.Update(item.Id, item)
         member this.All() = base.All()
+        member this.Exists(name: string): bool = 
+            let nameToSearch = name.ToLowerInvariant()
+            let filter = Builders<Company>.Filter.Where(fun x -> x.Name.ToLowerInvariant() = nameToSearch)
+            base.Collection.FindAsync(filter).Result.ToEnumerable().Any()
 
     static member overloadMap (map:BsonClassMap<Company>) = 
         map.MapProperty("Types").SetSerializer(CompanyTypeSerializer()) |> ignore
