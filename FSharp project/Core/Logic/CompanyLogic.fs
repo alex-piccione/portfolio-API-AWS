0fdd4a341c0564e584eadcbe62cfce40c74d78ef
Alex <alessandro.piccione.75@gmail.com>
2021-10-10 11:35:52 +0100

    create CompanyLogic

---
+++ FSharp project/Core/Logic/CompanyLogic.fs
@@ -0,0 +1,20 @@
+﻿namespace Portfolio.Core.Logic
+
+open System
+open Portfolio.Core
+open Portfolio.Core.Entities
+
+
+type CompanyLogic(companyRepository:ICompanyRepository) =
+
+    let normalize company =
+        {company with Id=Guid.NewGuid().ToString(); Name=company.Name.Trim()}
+
+    let validate company = 
+        if String.IsNullOrWhiteSpace( company.Name) then failwith "\"Name\" cannot be empty."
+        company
+
+    member this.Create(company:Company) =
+
+        let newCompany = validate <| normalize company
+        companyRepository.Create(newCompany)
