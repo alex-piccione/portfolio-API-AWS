2f75e483db8171ee0e5fa042f9c5b736a92f6a01
Alex <alessandro.piccione.75@gmail.com>
2021-10-10 21:20:58 +0100

    WIP

--- FSharp project/UnitTests/Core/Logic/CompanyLogic.tests.fs
+++ FSharp project/UnitTests/Core/Logic/CompanyLogic.tests.fs
@@ -2,12 +2,22 @@
 
 open System
 open NUnit.Framework
+open Foq.Linq
+open Portfolio.Core.Logic
+open Portfolio.Core
+
 
 
 type ``CompanyLogic Test`` () =
 
     [<Test>]
-    member this.Create() =
+    member this.``Create <when> name is duplicated <should> raises specific error``() =
+
+        let repository = Mock<ICompanyRepository>
+        let logic = CompanyLogic(repository)
+
+        // execute
+        //logic.Create(company)
 
         ()
 
