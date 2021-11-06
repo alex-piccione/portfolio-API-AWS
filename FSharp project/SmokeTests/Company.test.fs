58bfef128f45af1cbecf28c391d461d1accdb484
Alex <alessandro.piccione.75@gmail.com>
2021-11-06 09:45:02 +0000

    Update libraries and add Smoke Tests project

---
+++ FSharp project/SmokeTests/Company.test.fs
@@ -0,0 +1,23 @@
+module SmokeTests.Company
+
+open NUnit.Framework
+open FsUnit
+open Flurl.Http
+open AWSRequestSignerFlurl
+
+
+[<Test>]
+let ``Company All`` () =
+    let response = 
+        $"https://{secrets.url}/company/all"
+            .AllowAnyHttpStatus()
+            .Sign("GET", None, "execute-api", "eu-central-1")
+            .GetAsync().Result
+
+    let content = response.GetStringAsync().Result
+
+    if response.StatusCode <> 200
+    then
+        Assert.Fail(content)
+    else
+        content |> should not' (be NullOrEmptyString)
