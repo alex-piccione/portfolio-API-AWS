292ceb496b79e9510da7545199bc575505b1bce4
Alex <alessandro.piccione.75@gmail.com>
2021-11-06 13:01:23 +0000

    add missing file

---
+++ FSharp project/SmokeTests/secrets.fs
@@ -0,0 +1,22 @@
+﻿module secrets
+
+open Microsoft.Extensions.Configuration
+open System
+
+
+let configuration =
+    ConfigurationBuilder()
+        .AddUserSecrets("AWS.Portfolio")
+        .Build()
+
+let loadSecrets path env =
+    match configuration.[path] with
+    | null -> 
+        match Environment.GetEnvironmentVariable(env) with 
+        | null -> failwith $"""Secret with path "{path}" and Environment variable "{env}" are null."""
+        | value ->  value
+    | value ->  value
+
+let url =       loadSecrets "AWS:url" "URL"
+let accessKey = loadSecrets "AWS:access key" "KEY"
+let secretKey = loadSecrets "AWS:secret key" "SECRET"
