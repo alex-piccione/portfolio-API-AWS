67b3f317906b356746deb4c882bf8ff3dc6759e8
Alex <alessandro.piccione.75@gmail.com>
2022-03-24 08:24:01 +0000

    Create tests for CurrencyLogic Create method

--- FSharp project/UnitTests/Core/Logic/CurrencyLogic.tests.fs
+++ FSharp project/UnitTests/Core/Logic/CurrencyLogic.tests.fs
@@ -9,74 +9,93 @@ open Portfolio.Core
 open Portfolio.Core.Logic
 open Portfolio.Core.Entities
 
-type ``CompanyLogic Test`` () =
+type ``CurrencyLogic Test`` () =
 
     let newGuid () = Guid.NewGuid().ToString();
-    let aCompany:Company = { Id="TEST"; Name="Company"; Types=[Bank; Exchange]}
+    let aCurrency:Currency = { Code="AAA"; Name="Aaa"}
     let fundRepository = Mock.Of<IFundRepository>()
 
     [<Test>]
     member this.``Create``() =
-        let repository = Mock.Of<ICompanyRepository>()
-        let logic = CompanyLogic(repository, fundRepository) :> ICompanyLogic
-        let company:Company = { Id=""; Name="a name"; Types=[Bank]}
+        let repository = Mock.Of<ICurrencyRepository>()
+        let logic = CurrencyLogic(repository, fundRepository) :> ICurrencyLogic
 
         // execute
-        match logic.Create company with
-        | Ok comp -> 
-            comp.Id |> should not' (be NullOrEmptyString)
-            comp.Name |> should equal company.Name
-            comp.Types |> should equivalent company.Types
+        match logic.Create aCurrency with
+        | Ok c -> 
+            c.Code |> should equal aCurrency.Code
+            c.Name |> should equal aCurrency.Name
         | _ -> failwith "expected OK result"
 
     [<Test>]
-    member this.``Create [when] Name is empty [should] raise specific error``() =
-        let name = " "
-        let repository = Mock.Of<ICompanyRepository>()
-        let logic = CompanyLogic(repository, fundRepository) :> ICompanyLogic
-
-        let company:Company = {Id=""; Name=name; Types=[Bank]}
+    member this.``Create [when] Code is Empty [should] raise specific error``() =
+        let repository = Mock.Of<ICurrencyRepository>()
+        let logic = CurrencyLogic(repository, fundRepository) :> ICurrencyLogic
 
         // execute
-        match logic.Create company with
+        match logic.Create { aCurrency with Code = " "} with
+        | Error message -> message |> should contain $"Code cannot be empty."
+        | _ -> failwith "expected non valid result"
+
+    [<Test>]
+    member this.``Create [when] Name is Empty [should] raise specific error``() =
+        let repository = Mock.Of<ICurrencyRepository>()
+        let logic = CurrencyLogic(repository, fundRepository) :> ICurrencyLogic
+
+        // execute
+        match logic.Create { aCurrency with Name = " "} with
         | Error message -> message |> should contain $"Name cannot be empty."
         | _ -> failwith "expected non valid result"
 
     [<Test>]
-    member this.``Create [when] Name already exists [should] raise specific error``() =
-        let name = aCompany.Name
-        let repository = Mock<ICompanyRepository>()
-                             .SetupFunc(fun rep -> rep.Exists name).Returns(true)
+    member this.``Create [when] Code already exists [should] raise specific error``() =
+        let code = aCurrency.Code
+        let repository = Mock<ICurrencyRepository>()
+                             .SetupFunc(fun rep -> rep.ExistsWithCode code).Returns(true)
                              .Create()
-        let logic = CompanyLogic(repository, fundRepository) :> ICompanyLogic
+        let logic = CurrencyLogic(repository, fundRepository) :> ICurrencyLogic
 
         // execute
-        match logic.Create aCompany with
+        match logic.Create aCurrency with
         | Error message ->
-            message |> should contain $"A company with name \"{aCompany.Name}\" already exists."
+            message |> should contain $"A currency with code \"{code}\" already exists."
         | _ -> failwith "expected non valid result"
 
     [<Test>]
-    member this.``Update``() =
-        let name = "test"
-        let company:Company = {Id=newGuid(); Name=name; Types=[Bank]}
-
-        let repository = Mock<ICompanyRepository>()
-                                 .SetupFunc(fun rep -> rep.GetByName name).Returns(None)
-                                 //.SetupFunc(fun rep -> rep.Update company).Returns(ignore)
-                                 .Create()
-
-        let logic = CompanyLogic(repository, fundRepository) :> ICompanyLogic
+    member this.``Create [when] Name already exists [should] raise specific error``() =
+        let name = aCurrency.Name
+        let repository = Mock<ICurrencyRepository>()
+                             .SetupFunc(fun rep -> rep.ExistsWithName name).Returns(true)
+                             .Create()
+        let logic = CurrencyLogic(repository, fundRepository) :> ICurrencyLogic
 
         // execute
-        match logic.Update company with
-        | Ok c -> c.Id |> should equal company.Id
+        match logic.Create aCurrency with
+        | Error message ->
+            message |> should contain $"A currency with name \"{name}\" already exists."
+        | _ -> failwith "expected non valid result"
+(*
+     [<Test>]
+    member this.``Update``() =
+        let name = "test"
+        let currency = {aCurrency with Name=name}
+
+        let repository = Mock<ICurrencyRepository>()
+                            .SetupFunc(fun rep -> rep.ExistsWithName name).Returns(false)
+                            .Create()
+
+        let logic = CurrencyLogic(repository, fundRepository) :> ICurrencyLogic
+
+        // execute
+        match logic.Update currency with
+        | Ok c -> c.Id |> should equal currency.Id
         | _ -> failwith "expected OK"
 
-        verify <@ repository.Update company @> once
+        verify <@ repository.Update currency @> once
+        
 
     [<Test>]
-    member this.``Update [when] Name is empty [should] raise specific error``() =
+    member this.``Update [when] name is Empty [should] raise specific error``() =
         let name = " "
         let company:Company = {Id=Guid.NewGuid().ToString(); Name=name; Types=[Bank]}
         let repository = Mock.Of<ICompanyRepository>()
@@ -89,7 +108,7 @@ type ``CompanyLogic Test`` () =
         | _ -> failwith "expected Error"
 
     [<Test>]
-    member this.``Update [when] Name already exists [should] raise specific error``() =
+    member this.``Update [when] name Already Exists [should] raise specific error``() =
         let name = "test"
         let duplicatedName = name.ToUpper()
         let company:Company = {Id=Guid.NewGuid().ToString(); Name=name; Types=[Bank]}
@@ -105,6 +124,7 @@ type ``CompanyLogic Test`` () =
             message |> should contain $"A company with name \"{name}\" already exists."
         | _ -> failwith "expected non valid result"
 
+    
     [<Test>]
     member this.``Single [when] company exists`` () =
         let company = aCompany
@@ -163,3 +183,4 @@ type ``CompanyLogic Test`` () =
         | _ -> failwith "Expect result to be Error"
 
         verify <@ repository.Delete companyId @> never
+        *)
