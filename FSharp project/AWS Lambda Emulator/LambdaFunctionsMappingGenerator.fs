840e0f26643c216788d7d37169641a9acf0bee62
Alex <alessandro.piccione.75@gmail.com>
2022-04-30 10:46:37 +0100

    Update packages. Create Web application

---
+++ FSharp project/AWS Lambda Emulator/LambdaFunctionsMappingGenerator.fs
@@ -0,0 +1,18 @@
+﻿module LambdaFunctionsMappingGenerator
+
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Builder
+open Portfolio.Core.Entities
+
+
+let getCurrency (context:HttpContext) =
+    let currencies:Currency list = [
+        {Code="BTC"; Name="Bitcoin"}
+    ]
+    context.Response.WriteAsJsonAsync(currencies)
+       
+
+let generateMapping (app:WebApplication) =
+
+    app.MapGet("/currency", RequestDelegate getCurrency) |> ignore
+
--- FSharp project/AWS Lambda Emulator/Program.fs
+++ FSharp project/AWS Lambda Emulator/Program.fs
@@ -1,6 +1,32 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿// https://docs.microsoft.com/en-us/aspnet/core/migration/50-to-60?view=aspnetcore-6.0&tabs=visual-studio#new-hosting-model
 
+//open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Configuration
 
-// load configuration
-Configuration.
+//open Microsoft.AspNetCore  WebHost
+
+let url = "http://localhost:5000"
+
+//WebHost
+let builder = WebApplication.CreateBuilder()
+
+(*
+let configureHost (options:IConfigurationBuilder) =
+    let configuration:IConfiguration = {
+    }
+    options.AddConfiguration(configuration)
+*)
+
+//builder.Host.ConfigureHostConfiguration(configureHost)
+let app = builder.Build()
+
+let getHome (context:HttpContext) =    
+    context.Response.WriteAsync("hello")
+
+app.MapGet("/", RequestDelegate getHome) |> ignore
+LambdaFunctionsMappingGenerator.generateMapping(app)
+
+app.Urls.Add(url)
+app.Run()
